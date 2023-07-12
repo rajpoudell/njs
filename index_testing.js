b43c775ca7cb7b9c6c7552049d794065986a5b42
Raj Poudel <raj.poudeel@gmail.com>
2023-07-12 17:27:50 +0545

    init

---
+++ index_testing.js
@@ -0,0 +1,13 @@
+// const fs = require('fs');
+// const os = require('os');
+
+//os(operating system)
+// console.log(os.totalmem());
+
+
+//fs(filesystem)
+// const a = "this is writen by raj";
+// fs.writeFileSync('./simple.txt',a);
+
+// console.log("im first")
+
