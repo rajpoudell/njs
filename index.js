83ccd67fa15e27faf38f6d53729327d5c7b1b5a0
Raj Poudel <raj.poudeel@gmail.com>
2023-07-12 18:07:29 +0545

    added comment

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const hostname ="localhost"
 // (request,response,next(optional for local server project))
 
 const server = http.createServer((req,res)=>{
-    res.end("working")
+    // res.end("working")
+    res.end("<h1>hello server</h1>")
 });
 
+// server.listen(2000,"localhost",() => {}
+// listen() method to start a server. Let's break down the code and its functionality:
+// The () => { ... } is an arrow function that serves as a callback function. 
+// It contains the code that will execute once the server starts successfully.
 server.listen(PORT,hostname,() => {
     console.log(`server is working on http://${hostname}:${PORT}`)
 });
