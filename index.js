ac0b4340e08bfa19be815ff723ee1a5bc320d63b
Raj Poudel <raj.poudeel@gmail.com>
2023-07-14 09:46:23 +0545

    space

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ console.log(req.url);
 // listen() method to start a server. Let's break down the code and its functionality:
 // The () => { ... } is an arrow function that serves as a callback function. 
 // It contains the code that will execute once the server starts successfully.
+
 server.listen(PORT,hostname,() => {
     console.log(`server is working on http://${hostname}:${PORT}`)
 });
